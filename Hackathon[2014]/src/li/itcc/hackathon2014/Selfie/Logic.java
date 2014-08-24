e47b4e1a993f1f50df6613e1b3d9ab0aabf3d04f
thomasE1993 <thomas.elsensohn@gmail.com>
2014-08-24 06:54:37 +0200

    update

--- Hackathon[2014]/src/li/itcc/hackathon2014/Selfie/Logic.java
+++ Hackathon[2014]/src/li/itcc/hackathon2014/Selfie/Logic.java
@@ -1,26 +1,24 @@
+
 package li.itcc.hackathon2014.Selfie;
 
 import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.logging.Logger;
 
-import li.itcc.hackathon2014.R;
 import android.app.Activity;
-import android.app.Application;
 import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 
@@ -28,7 +26,7 @@ public class Logic {
     Activity activity;
     Context context;
     Uri outputFileUri;
-    String _path = "data/data/li.itcc.hackathon2014.Selfie/";
+    String _path = context.getFilesDir().getPath();
 
     public Logic(Activity ac, Context con) {
         activity = ac;
@@ -38,8 +36,8 @@ public class Logic {
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     public Uri TakePictureIntent() {
-        File file = new File(_path);
-        outputFileUri = Uri.fromFile(file);
+        
+//        outputFileUri = Uri.fromFile(image);
 
         Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
@@ -51,39 +49,9 @@ public class Logic {
 
     }
 
-    public void addWatermark(String pic, int watermark) {
+    public void addWatermark(Bitmap bitmap1, int watermark) {
         // definieren .. pfad zum bitmap
-        Bitmap bitmap1 = null;
         Bitmap bitmap2 = BitmapFactory.decodeResource(context.getResources(), watermark);
-
-        InputStream is = null;
-        BufferedInputStream bis = null;
-
-        try {
-            URLConnection conn = new URL(pic).openConnection();
-            conn.connect();
-            is = conn.getInputStream();
-            bis = new BufferedInputStream(is, 80000);
-            bitmap1 = BitmapFactory.decodeStream(bis);
-        } catch (Exception e)
-        {
-            e.printStackTrace();
-        } finally {
-            if (bis != null) {
-                try {
-                    bis.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (is != null) {
-                try {
-                    is.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
 
         // create white Image
         Bitmap resultBitmap = Bitmap.createBitmap(bitmap1.getWidth(), bitmap1.getHeight(),
@@ -99,7 +67,34 @@ public class Logic {
 
         // draw watermark
         c.drawBitmap(bitmap2, 0, 0, p);
-        //TODO: Save Canvas: resultBitmap.compress(Bitmap.CompressFormat.JPEG, 100, new FileOutputStream(new File(Activity.)));
 
-    } // TO DO: bmp 2 jpeg und speichern
+//        try {
+//            resultBitmap.compress(Bitmap.CompressFormat.JPEG, 100, null);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+
+    public Bitmap BitmapSave(Bitmap image) {
+        String root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString()+ "/saved_images";
+        File myDir = new File(root);
+        myDir.mkdirs();
+////        String photoName = "image" + diveNum + ".jpg";
+////        File file = new File(root + "/" + photoName);
+//        
+//        if (file.exists()) {
+//            file.delete();
+//        }
+//        try {
+//            FileOutputStream out = new FileOutputStream(file);
+//            image.compress(Bitmap.CompressFormat.JPEG, 90, out);// compress
+//            // output
+//            out.flush();
+//            out.close(); 
+//        } catch(Exception ex) {
+//            ex.printStackTrace();
+//        }
+    return null;
+    }
 }
