90541a9e847659b9e64d004a700fe7cd17087a08
thomasE1993 <thomas.elsensohn@gmail.com>
2014-09-08 23:57:30 +0200

    Beta FileChooser. Without Click Handler

--- Hackathon[2014]/src/li/itcc/lieventure/filechooser/FilechooserActivity.java
+++ Hackathon[2014]/src/li/itcc/lieventure/filechooser/FilechooserActivity.java
@@ -1,19 +1,47 @@
 
 package li.itcc.lieventure.filechooser;
 
+import java.io.File;
+import java.util.ArrayList;
 import li.itcc.lieventure.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
-import android.view.View;
 
 public class FilechooserActivity extends Activity {
+
+    /**
+     * @param files (alle Files eines Verzeichnisses)
+     * @return List<String>
+     */
+    public ArrayList<String> fromFileToString(File[] files) {
+        ArrayList<String> fileList = new ArrayList<String>();
+
+        for (File file : files) {
+            fileList.add(file.getName());
+        }
+        return fileList;
+    }
+
+    public static void showFiles(File[] files) {
+        for (File file : files) {
+            if (file.isDirectory()) {
+
+                System.out.println("Directory: " + file.getName());
+            } else {
+                System.out.println("File: " + file.getName());
+            }
+        }
+    }
+
     String[] values = new String[] {
             "Test1",
             "Test2",
@@ -22,19 +50,26 @@ public class FilechooserActivity extends Activity {
             "5",
             "1234567890123456789012345678901234567890"
     };
-    
+
     ListView list = null;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        File rootDir = null;
+        File[] rootFiles = null;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filechooser);
 
-        list = (ListView) findViewById(R.id.list);
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        rootDir = new File(Environment.getExternalStorageDirectory().getPath());
+        rootFiles = rootDir.listFiles();
 
-        
+        ArrayList<String> FileListName = fromFileToString(rootFiles);
+
+        list = (ListView) findViewById(R.id.list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, FileListName);
+
         list.setAdapter(adapter);
 
         // Item Click Listener
